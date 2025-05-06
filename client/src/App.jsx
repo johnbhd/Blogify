99e957a27c6b07626381be232cd11787e475043e
johnbhd <villegasjohnb18@gmail.com>
2025-05-06 19:44:44 +0800

    done sign in next sign up and loader. Jesus loves you - 2 Timothy 1:7

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
+import AuthPage from "./pages/AuthPage"
 
-function App() {
- 
+function App() { 
   return (
     <>
-      <div className="bg-black w-full">
-        <h1 className="text-white text-center">hello</h1>
-      </div>
+      <AuthPage/>
     </>
   )
 }
